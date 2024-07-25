d1a59ac6560d6ce5bb6b186316adfe929d20254a
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-07-26 01:10:03 +0530

    error solve

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import wasm from 'vite-plugin-wasm';
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 
 // https://vitejs.dev/config/
